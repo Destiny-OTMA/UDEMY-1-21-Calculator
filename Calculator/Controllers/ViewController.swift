e6ec81a5969cf2e672651486b5b8f51551c7f410
Destiny Sopha <destiny@sopha.org>
2019-08-17 12:14:57 -0400

    Added the code to check and disallow multiple decimal points in the display

--- Calculator/Controllers/ViewController.swift
+++ Calculator/Controllers/ViewController.swift
@@ -9,52 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var displayLabel: UILabel!
   
-    private var isFinishedTypingNumber: Bool = true
+  @IBOutlet weak var displayLabel: UILabel!
+  
+  private var isFinishedTypingNumber: Bool = true
+  
+  
+  @IBAction func calcButtonPressed(_ sender: UIButton) {
     
+    //What should happen when a non-number button is pressed
     
-    @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
+    isFinishedTypingNumber = true
     
-      isFinishedTypingNumber = true
+    guard let number = Double(displayLabel.text!) else {
+      fatalError("Cannot convert display label text to a Double.")
+    }
     
-      guard let number = Double(displayLabel.text!) else {
-        fatalError("Cannot convert display label text to a Double.")
-      }
-      
-      if let calcMethod = sender.currentTitle {
-        if calcMethod == "AC" {
-          displayLabel.text = "0"
-        } else if calcMethod == "+/-" {
-          displayLabel.text = String(number * -1)
-        } else if calcMethod == "%" {
-          displayLabel.text = String(number / 100)
-        }
+    // Code to handle the black buttons
+    if let calcMethod = sender.currentTitle {
+      if calcMethod == "AC" {
+        displayLabel.text = "0"
+      } else if calcMethod == "+/-" {
+        displayLabel.text = String(number * -1)
+      } else if calcMethod == "%" {
+        displayLabel.text = String(number / 100)
       }
     }
-
+  }
   
-    @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-      //What should happen when a number is entered into the keypad
+  
+  @IBAction func numButtonPressed(_ sender: UIButton) {
+    
+    //What should happen when a number is entered into the keypad
+    
+    
+    if let numValue = sender.currentTitle {
       
-
-      if let numValue = sender.currentTitle {
+      if isFinishedTypingNumber {
+        displayLabel.text = numValue
+        isFinishedTypingNumber = false
+      } else {
         
-        if isFinishedTypingNumber {
-          displayLabel.text = numValue
-          isFinishedTypingNumber = false
-        } else {
-          displayLabel.text = displayLabel.text! + numValue
+        // Check to see if a decimal point has already been entered
+        // and if it already has, then do not allow another one.
+        if numValue == "." {
           
+          guard let currentDisplayValue = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double!.")
+          }
+          
+          let isInt = floor(currentDisplayValue) == currentDisplayValue
+          
+          if !isInt {
+            return
+          }
+
         }
-
+        
+        displayLabel.text = displayLabel.text! + numValue
+        
       }
-    
+      
     }
-
+    
+  }
+  
 }
 
